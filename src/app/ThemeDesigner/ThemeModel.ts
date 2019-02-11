6084b71b037802555e5e76b40bde0cafb89a8005
Marco Bonati <marco.bonati@vipera.com>
2019-02-11 11:06:05 +0100

    Added new ControlKit and theme model

---
+++ src/app/ThemeDesigner/ThemeModel.ts
@@ -0,0 +1,111 @@
+
+
+export interface ThemeItem {
+  description: string
+}
+
+export class ThemeGroup implements ThemeItem {
+  items: ThemeItem[];
+  description: string;
+}
+
+export class ThemeModel extends ThemeGroup {
+  description: string;
+}
+
+
+
+const LOG_TAG = '[DefaultThemeModel]';
+
+export class ThemeColorItem implements ThemeItem {
+
+  private _value: string;
+
+  constructor(public name: string,
+    public description: string,
+    public cssPropertyName: string,
+    private themeWrapper: any,
+    private logger: NGXLogger) {
+      this._value = this.getColorProperty(cssPropertyName);
+  }
+
+  public get value(){
+    return this._value;
+  }
+
+  public set value(colorValue:string) {
+    this._value = colorValue;
+    this.themeWrapper.style.setProperty(this.cssPropertyName, colorValue);
+  }
+
+  private getColorProperty(variableName: string): string {
+    var style = getComputedStyle(this.themeWrapper);
+    var ret = style.getPropertyValue(variableName).trim();
+    return ret;
+  }
+
+
+}
+
+
+export class DefaultThemeModel implements ThemeModel {
+
+  private themeWrapper: any;
+
+  items: ThemeItem[] = [];
+  description: "Theme Model";
+
+  constructor(private logger: NGXLogger, private document: any){
+    this.logger.debug(LOG_TAG, 'Initializing' );
+    console.log("@Inject(DOCUMENT) private document ", document);
+    this.themeWrapper = this.document.querySelector('app-root');
+    this.createModel();
+    console.log("this.themeWrapper:", this.themeWrapper);
+  }
+
+  private createModel(){
+    this.items.push(this.createMainGroup());
+  }
+
+  private createMainGroup(): ThemeGroup {
+    let ret = new ThemeGroup();
+    ret.description = "Main";
+    ret.items = [
+
+      // Main Colors
+      this.createColorItem('background', 'Main Background', '--main-background-color'),
+
+      // Header Colors
+      this.createColorItem('header', 'Header Background', '--header-background-color'),
+      this.createColorItem('header', 'Header Color', '--header-color'),
+      this.createColorItem('header', 'Header Hover', '--header-color-hover')
+
+    ];
+    return ret;
+  }
+
+  private createColorItem(name: string, description:string, cssPropertyName: string): ThemeColorItem {
+    return new ThemeColorItem(name, description, cssPropertyName, this.themeWrapper, this.logger);
+  }
+
+}
+
+
+import { Inject, destroyPlatform } from '@angular/core';
+import { NGXLogger } from 'web-console-core';
+import { DOCUMENT } from '@angular/common';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+
+export class ThemeModelBuilder {
+
+    constructor(private logger: NGXLogger, @Inject(DOCUMENT) private document: any){
+      this.logger.debug(LOG_TAG, 'Initializing Theme Builder' );
+    }
+
+    public createModel(): ThemeModel {
+      let model: ThemeModel = new DefaultThemeModel(this.logger, this.document);
+      return model;
+    }
+
+
+}
--- src/app/ThemeDesigner/WAThemeDesignerService.ts
+++ src/app/ThemeDesigner/WAThemeDesignerService.ts
@@ -1,3 +1,4 @@
+import { ThemeModelBuilder, ThemeModel, ThemeItem, ThemeGroup, ThemeColorItem } from './ThemeModel';
 import { Injectable, Inject } from '@angular/core';
 import { NGXLogger } from 'web-console-core';
 import { GUI } from "dat-gui";
@@ -18,6 +19,7 @@ interface ColorItemDef {
 export class WAThemeDesignerService {
 
   private themeWrapper: any;
+  private themeModel: ThemeModel;
 
     constructor(private logger: NGXLogger, @Inject(DOCUMENT) private document: any){
       console.log("@Inject(DOCUMENT) private document ", document);
@@ -35,22 +37,7 @@ export class WAThemeDesignerService {
 
         let colorStr = "#ffae23";//this.getColorProperty('--header-background-color');
 
-        this.obj = {
-          main: {
-            "Background": this.getColorProperty('--main-background-color'),
-            "Title1" : this.getColorProperty('--title-1-color')
-          },
-            header: {
-                "Background": this.getColorProperty('--header-background-color'),
-                "Color" : this.getColorProperty('--header-color'),
-                "ColorHover" : this.getColorProperty('--header-color-hover')
-            },
-            Export: () =>{
-              let test = this.getColorProperty('--header-background-color');
-              alert(test);
-              console.log("Color:",test)
-            }
-          };
+        this.obj = this.createThemeObject();
 
           let mainFolder = this.createEmptyFolder(gui, "Main");
 
@@ -70,6 +57,25 @@ export class WAThemeDesignerService {
 
           this.logger.debug(LOG_TAG, 'show done' );
 
+    }
+
+    private createThemeObject() : any {
+      return {
+        main: {
+          "Background": this.getColorProperty('--main-background-color'),
+          "Title1" : this.getColorProperty('--title-1-color')
+        },
+          header: {
+              "Background": this.getColorProperty('--header-background-color'),
+              "Color" : this.getColorProperty('--header-color'),
+              "ColorHover" : this.getColorProperty('--header-color-hover')
+          },
+          Export: () =>{
+            let test = this.getColorProperty('--header-background-color');
+            alert(test);
+            console.log("Color:",test)
+          }
+        };
     }
 
     private createEmptyFolder(gui: GUI, folderName: string): GUI {
@@ -94,19 +100,41 @@ export class WAThemeDesignerService {
     }
 
     public show(){
-      var obj = {
-        number : 0,
-        string : 'abc',
-        color: '#ff00aa'
-    };
-      var controlKit = new ControlKit();
-	    controlKit.addPanel({ fixed: false, width: 400, label: "Theme Editor", dock: false, align: "right", position: [0,50]})
-	        .addGroup({label:'Main Colors'})
-	            .addSubGroup({label: 'Commons'})
-                  .addNumberInput(obj,'number')
-                  .addColor(obj,'color',{colorMode:'hex', label: 'Title 1 Color'})
-	                .addStringInput(obj,'string');
+        this.themeModel = new ThemeModelBuilder(this.logger, this.document).createModel();
+        this.logger.debug(LOG_TAG, 'ThemeModelBuilder new model:',this.themeModel);
+        let controlKit = new ControlKit({opacity:0.8 });
+        let panel = controlKit.addPanel({ fixed: false, width: 400, label: "Theme Editor", dock: false, align: "right", position: [0,50]});
+
+        for (let i=0;i<this.themeModel.items.length;i++){
+          let item: ThemeItem = this.themeModel.items[i];
+          if (item instanceof ThemeGroup){
+            this.addThemeGroupTo(panel, item);
+          } else if (item instanceof ThemeColorItem) {
+            this.addColorTo(panel, item);
+          }
+        }
+
+        panel.addButton('Export',() => { alert("TODO!!"); });
+
+    }
+
+    private addThemeGroupTo(ui: any, group: ThemeGroup) {
+      let subUI = ui.addGroup( { label: group.description, enable: false } );
+      for (let i=0;i<group.items.length;i++){
+        let item: ThemeItem = group.items[i];
+        if (item instanceof ThemeGroup){
+          this.addThemeGroupTo(subUI, item);
+        } else if (item instanceof ThemeColorItem) {
+          this.addColorTo(subUI, item);
+        }
+      }
+    }
+
+    private addColorTo(ui: any, colorItem: ThemeColorItem) {
+      ui.addColor(colorItem, 'value', { colorMode:'hex', label: colorItem.description });
     }
 
 
+
 }
+
