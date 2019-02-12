521e1444009795c62c1a01e30eddcd6d748e736f
Marco Bonati <marco.bonati@vipera.com>
2019-02-12 15:26:26 +0100

    Minor fixes

--- src/app/ThemeDesigner/toolbox/color-picker-field-component/wa-theme-designer-color-picker.ts
+++ src/app/ThemeDesigner/toolbox/color-picker-field-component/wa-theme-designer-color-picker.ts
@@ -2,6 +2,10 @@ import { ElementRect } from '@progress/kendo-popup-common';
 import { CSSColorHelper } from './helpers/CSSColorHelper';
 import { Component, ViewEncapsulation, ViewChild, Output, Input, ElementRef } from '@angular/core';
 import { ColorEvent, Color,  RGBA } from 'ngx-color';
+import * as __color_string from 'color-string';
+
+const colorString = __color_string;
+
 
 @Component({
   selector: 'wa-theme-designer-color-picker',
@@ -11,10 +15,13 @@ import { ColorEvent, Color,  RGBA } from 'ngx-color';
 })
 export class WAThemeDesignerColorPicker {
 
-  @Input() @Output() color: Color;
+  @Input() @Output() colorRaw: Color;
 
   @ViewChild("op") overlayPanel: any;
   @ViewChild("pickerButton") pickerButton:ElementRef;
+  @ViewChild("colorPanel") colorPanel:ElementRef;
+
+  public colorString:string;
 
   constructor(){
   }
@@ -27,16 +34,48 @@ export class WAThemeDesignerColorPicker {
   }
 
   handleChange($event: ColorEvent) {
-    this.color = $event.color;
+    this.colorRaw = $event.color;
   }
 
-  @Output()
-  public get colorCSS():string {
-    return CSSColorHelper.toCSSString(this.color);
+  @Input()
+  public get color():string {
+    return CSSColorHelper.toCSSString(this.colorRaw);
   }
 
-  public set colorCSS(value:string) {
-    //color_parse(value);
+  @Input()
+  public set color(value:string) {
+    this.colorString = this.colorString;
+    if (value){
+      let colorComp = colorString.get(value);
+      console.log("SET COLOR ", value, colorComp);
+      let hexValue = colorString.to.hex(colorComp.value);
+      let rgbaValue = colorString.to.rgb(colorComp.value);
+      let hslValue = colorString.to.hsl(colorComp.value);
+      console.log("SET COLOR ", value, hexValue, rgbaValue, hslValue);
+      this.colorRaw = {
+        hex: hexValue,
+        rgb: {
+          r: colorComp.value[0],
+          g: colorComp.value[1],
+          b: colorComp.value[2],
+          a: colorComp.value[3]
+        },
+        hsl: {
+          a: 0,
+          h: 0,
+          l: 0,
+          s: 0
+        },
+        hsv: {
+          a:0,
+          h:0,
+          s:0,
+          v:0
+        },
+        oldHue: 0,
+        source: ""
+      }
+    }
   }
 
   onInputDblClick(event){
